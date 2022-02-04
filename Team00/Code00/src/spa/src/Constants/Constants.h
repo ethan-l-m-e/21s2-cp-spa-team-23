0b98e7ac22954f97ef819ff823a935fd8a317d39
tin_hong_wen <e03256506@u.nus.edu>
2022-02-04 16:02:27 +0800

    modified constants

---
+++ Team00/Code00/src/spa/src/Constants/Constants.h
@@ -0,0 +1,17 @@
+#pragma once
+using namespace std;
+
+// switch statements
+int const BASE_CASE = 0;
+int const PROCEDURE = 1;
+int const ASSIGN = 2;
+int const ERROR = 3;
+int const WHILE = 4;
+int const IF_ELSE = 5;
+int const READ = 6;
+int const OPERATOR = 7;
+int const PRINT = 8;
+int const CALL = 9;
+int const COND_EXPR = 10;
+int const EXPR_TERM = 11;
+
