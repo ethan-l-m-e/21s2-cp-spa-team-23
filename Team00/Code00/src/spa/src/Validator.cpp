964875f9fbb5a3bdff75913cacd8be94d9302458
tin_hong_wen <e03256506@u.nus.edu>
2022-02-05 13:28:21 +0800

    added comments

--- Team00/Code00/src/spa/src/Validator.cpp
+++ Team00/Code00/src/spa/src/Validator.cpp
@@ -8,6 +8,21 @@
 #include "Validator.h"
 using namespace std;
 
+
+/**
+ * IMPORTANT NOTE this algorithm DOES NOT CHECK:
+ *
+ * - brackets after closure
+ * (e.g. '(x + 1) * (y - 1)'. does not check the latter brackets (y - 1))
+ *
+ * - other types of brackets other than the one specified
+ * (e.g. '( ss {ddd) sss }'. this will be counted as correct by the algo, but it's obviously wrong
+ *
+ * these 2 problems shall be resolved by the main recursion in Parser.cpp by via gradual trimming/partitioning
+ * e.g. 'procedure a {}} procedure b {}'
+ * after trimming the correct portion of the code, left with '} procedure b {}'.
+ * The stray '}' will throw an error during identification
+ */
 bool Validator::checkParenthesesCorrectness(string code, string brackets) {
     int count = 0;
     bool startedCount = false;
