9b5aac50581e0e99da00f5547e6569ee03d33ef1
tin_hong_wen <e03256506@u.nus.edu>
2022-02-06 18:04:33 +0800

    modified partition to handle empty second half

--- Team00/Code00/src/spa/src/StringFormatter.cpp
+++ Team00/Code00/src/spa/src/StringFormatter.cpp
@@ -1,17 +1,23 @@
 //
 // Created by Lucas Tai on 4/2/22.
 //
+#include <iostream>
 
 #include "StringFormatter.h"
 #include "Constants/Constants.h"
+
+using namespace std;
+
 Partition StringFormatter::Trim(std::string sourceCode, int type) {
-    std::string trimmedCode;
+    string trimmedCode;
     Partition finalStrings = Partition();
     switch(type) {
         case ASSIGN:{
             int pos = sourceCode.find('\n');
             trimmedCode = sourceCode.substr(0, pos);
-            std::string codeToRecurse = sourceCode.substr(pos + 1,sourceCode.size()); // +1 to get rid of the \n space
+            string codeToRecurse;
+            if (pos == -1)      codeToRecurse = "";     //empty string for latter half
+            else       codeToRecurse = sourceCode.substr(pos + 1,sourceCode.size()); // +1 to get rid of the \n space
             finalStrings = Partition(trimmedCode,codeToRecurse);
             break;
         }
