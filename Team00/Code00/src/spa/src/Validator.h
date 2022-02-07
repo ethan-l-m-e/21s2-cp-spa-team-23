ef3b8e40ccb7ff3b5171450a5cfb282c163edee7
tin_hong_wen <e03256506@u.nus.edu>
2022-02-07 09:30:57 +0800

    mod validator header file

--- Team00/Code00/src/spa/src/Validator.h
+++ Team00/Code00/src/spa/src/Validator.h
@@ -9,9 +9,8 @@
 using namespace std;
 
 class Validator {
-    public:
-        static bool checkParenthesesClosure(string code, string brackets);
-
+public:
+    static bool checkParenthesesClosure(string code, string brackets);
 };
 
 
