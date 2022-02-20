3d538697ad6f43ac2d604ae66fb5b58f5230a8b7
sruthisarav <sruthisarav2@gmail.com>
2022-02-20 20:01:09 +0800

    Add 500s to queries

--- Team00/Code00/src/autotester/src/TestWrapper.cpp
+++ Team00/Code00/src/autotester/src/TestWrapper.cpp
@@ -22,7 +22,7 @@ TestWrapper::TestWrapper() {
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
-    SourceProcessor::run(filename);
+//    SourceProcessor::run(filename);
 
     // call your parser to do the parsing
     // ...rest of your code...
