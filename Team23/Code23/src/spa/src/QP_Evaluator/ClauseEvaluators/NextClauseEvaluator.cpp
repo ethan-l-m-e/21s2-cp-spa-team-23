342f37fcac9d1fb100429d885b1411276670e846
tin_hong_wen <e03256506@u.nus.edu>
2022-03-11 00:40:26 +0800

    added default methods, not working atm

--- Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/NextClauseEvaluator.cpp
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/NextClauseEvaluator.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "NextClauseEvaluator.h"
+
+bool NextClauseEvaluator::isRelation(string left, string right) {
+    //return pkb->isNext(left, right)
+    return false;
+}
+
+unordered_set<std::string> NextClauseEvaluator::getLeftSynonymValue(std::string right) {
+    //return pkb-getPreviousNodeOf(right);
+    return {};
+}
+
+unordered_set<std::string> NextClauseEvaluator::getRightSynonymValue(std::string left) {
+    //return pkb-getNextNodeOf(left);
+    return {};
+}
+
+pair<DesignEntity, DesignEntity> NextClauseEvaluator::getWildcardType () {
+    return make_pair(DesignEntity::STMT, DesignEntity::STMT);
+}
