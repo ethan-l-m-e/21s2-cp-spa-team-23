c6e9177f4857cdc6e6776c7f9f143b0d90841e70
ethan-l-m-e <ethan.l.m.e@icloud.com>
2022-03-12 06:10:06 +0800

    add rest of test cases to script add missing 5000 to procedures.txt

--- Team23/Tests23/test_script.sh
+++ Team23/Tests23/test_script.sh
@@ -1,2 +1,11 @@
 
 ../Code23/cmake-build-debug/src/autotester/autotester follows_source.txt follows_queries.txt ../Code23/tests/follows_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester modifies_source.txt modifies_queries.txt ../Code23/tests/modifies_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester nested_source.txt nested_queries.txt ../Code23/tests/nested_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester parent_source.txt parent_queries.txt ../Code23/tests/parent_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester pattern_source.txt pattern_queries.txt ../Code23/tests/pattern_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester procedures_source.txt procedures_queries.txt ../Code23/tests/procedures_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester select_source.txt select_queries.txt ../Code23/tests/select_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester test_source.txt test_queries.txt ../Code23/tests/test_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester tuples_source.txt tuples_queries.txt ../Code23/tests/tuples_out.xml
+../Code23/cmake-build-debug/src/autotester/autotester uses_source.txt uses_queries.txt ../Code23/tests/uses_out.xml
