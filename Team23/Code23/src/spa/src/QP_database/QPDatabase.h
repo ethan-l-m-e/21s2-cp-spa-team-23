2c72160b6c544913cbee8721343ddc6e7ea275cd
tin_hong_wen <e03256506@u.nus.edu>
2022-03-12 20:23:24 +0800

    completed CFG operator logic without optimisation, not yet string to an evaluator

---
+++ Team23/Code23/src/spa/src/QP_database/QPDatabase.h
@@ -0,0 +1,16 @@
+//
+// Created by Tin Hong Wen on 10/3/22.
+//
+/**
+ * NOTE: This is for optimisation purposes
+ */
+#ifndef SPA_QPDATABASE_H
+#define SPA_QPDATABASE_H
+
+
+class QPDatabase {
+
+};
+
+
+#endif //SPA_QPDATABASE_H
