494a9a1f28cb18c56d8c4f30a2498d148140a404
tin_hong_wen <e03256506@u.nus.edu>
2022-03-16 13:59:49 +0800

    modified patternClause

--- Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/PatternClauseEvaluator.h
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/PatternClauseEvaluator.h
@@ -12,7 +12,9 @@
 
 class PatternClauseEvaluator : public ClauseEvaluator {
 public:
-    PatternClauseEvaluator (SynonymType synonymType, std::vector<Argument> args, PKB* pkb, Query* query): ClauseEvaluator(std::move(args), pkb, query) {}
+    PatternClauseEvaluator (SynonymType synonymType, std::vector<Argument> args, PKB* pkb, Query* query): ClauseEvaluator(std::move(args), pkb, query) {
+        SynonymType synType = synonymType;
+    }
     bool evaluateClause(ResultTable* resultTable) override;
 protected:
 
@@ -22,12 +24,19 @@ protected:
     bool rightIsWildCard();
     bool rightIsPartWildCard();
     bool rightIsIdent();
+    SynonymType synType;
 
     Argument syn = argList[0];
     Argument arg1 = argList[1];
     Argument arg2 = argList[2];
     Argument arg3;
     // get argument 3 if is if-node
+    bool evaluateAssign(ResultTable *resultTable);
+    bool evaluateWhile(ResultTable *resultTable);
+    bool evaluateIf(ResultTable *resultTable);
+
+    void constructResults(vector<ResultItem> tuplesResults, vector<ResultItem> stmtNumResults);
+
 };
 
 
