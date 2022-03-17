5937c7ed33334acb18a566de3df2fc71e7aca310
Tianyi <jonwang129@gmail.com>
2022-03-18 05:37:20 +0800

    refactor result clause evaluator

--- Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/ResultClauseEvaluator.cpp
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/ResultClauseEvaluator.cpp
@@ -12,43 +12,47 @@ bool ResultClauseEvaluator::evaluateClause(ResultTable* resultTable) {
     }
 
     vector<int> projections;
-    if (resultTable->getBooleanResult()) evaluateSelectedSynonyms(&projections, resultTable);
+    if (resultTable->getBooleanResult()) projectSelectedSynonyms(&projections, resultTable);
     resultTable->rearrangeSynonyms(projections);
     return true;
 
 }
 
-void ResultClauseEvaluator::evaluateSelectedSynonyms(vector<int>* projections, ResultTable* resultTable) {
+/**
+ * Project the result by traversing the selected synonyms.
+ * @param projections  a vector of integers that represents the column index and order for the final result
+ * @param resultTable  pointer to the result table used
+ */
+void ResultClauseEvaluator::projectSelectedSynonyms(vector<int>* projections, ResultTable* resultTable) {
     auto header = resultTable->getHeader();
     for (Argument synonym: query->getSelectedSynonyms()) {
+
+        // get synonym value and attribute reference.
+        string synonymValue;
         std::pair<string, AttrName> attrRef;
-        std::string synonymValue;
-        if (synonym.argumentType == ArgumentType::ATTR_REF) {
-            attrRef = std::get<std::pair<string, AttrName>>(synonym.argumentValue);
-            synonymValue = attrRef.first;
-        } else {
-            synonymValue = std::get<std::string>(synonym.argumentValue);
-        }
+        unpackSynonym(synonym, &attrRef, &synonymValue);
+        
+        // find synonym in result table.
         auto it = std::find(header->begin(), header->end(), synonymValue);
         auto index = std::distance(header->begin(), it);
-        if (it == header->end()) appendNewSynonym(synonymValue, resultTable);
-        if (!attrRef.first.empty()) {
-            std::string(ResultClauseEvaluator::*func) (std::string) = nullptr;
-            std::string name;
-            if (applyAttrRef(attrRef, &func, &name)) {
-                auto it1 = std::find(header->begin(), header->end(), name);
-                long newIndex = std::distance(header->begin(), it1);
-                if (it1 == header->end()) {
-                    std::vector<std::string> newColumn = getMapping((*resultTable->getList())[index], func);
-                    resultTable->appendColumn(name, newColumn);
-                }
-                index = newIndex;
-            }
-        }
+
+        //merge synonyms to result table if synonym is not in the table.
+        if (it == header->end()) 
+            appendNewSynonym(synonymValue, resultTable);
+
+        //handle attribute reference.
+        if (synonym.argumentType == ArgumentType::ATTR_REF) 
+            updateTableForAttrReference(attrRef, &index, resultTable);
+
         projections->emplace_back(index);
     }
 }
 
+/**
+ * Append a new synonym to the result table by doing a cross product for all the possible values from the synonym.
+ * @param synonymValue  the string value of the synonym for identifying the synonym in declarations
+ * @param resultTable  pointer to the result table used
+ */
 void ResultClauseEvaluator::appendNewSynonym(string synonymValue, ResultTable* resultTable){
     unordered_set<std::string> set = getAllType(query->getSynonymType(synonymValue));
     Result result = {
@@ -60,7 +64,14 @@ void ResultClauseEvaluator::appendNewSynonym(string synonymValue, ResultTable* r
     resultTable->mergeResultToTable(result);
 }
 
-bool ResultClauseEvaluator::applyAttrRef(std::pair<string, AttrName>& attrRef, std::string (ResultClauseEvaluator::**func)(std::string), std::string *name) {
+/**
+ * Apply attribute reference and populate the header name and caller function for attributes with different value from default.
+ * @param attrRef  the attribute reference struct with a string identifier and an attribute name
+ * @param func  pointer to the function pointer to be used for getting the mapping of the attribute for each value
+ * @param name  pointer to the name of the new header to be added to result table for identifying the attribute column
+ * @return  returns a boolean value representing whether name and function pointer have been populated
+ */
+bool ResultClauseEvaluator::applyAttrRef(std::pair<string, AttrName>& attrRef, std::string (ResultClauseEvaluator::**func)(std::string&), std::string *name) {
     if(query->findEntityType(attrRef.first) == DesignEntity::READ && attrRef.second == AttrName::VAR_NAME) {
         *name = attrRef.first + ".varName";
         *func = &ResultClauseEvaluator::getVarRead;
@@ -76,22 +87,64 @@ bool ResultClauseEvaluator::applyAttrRef(std::pair<string, AttrName>& attrRef, s
     return true;
 }
 
-std::vector<std::string> ResultClauseEvaluator::getMapping(std::vector<std::string>& lst, std::string (ResultClauseEvaluator::*func) (std::string)) {
+/**
+ * Get the mapped new values for the attribute reference from the values in an existing column
+ * @param lst  reference to the list in the result table
+ * @param func  function pointer indicating the function to be applied for getting the mapped value
+ * @return  a new list with the values for the attribute corresponding to each value
+ */
+std::vector<std::string> ResultClauseEvaluator::getMapping(std::vector<std::string>& lst, std::string (ResultClauseEvaluator::*func) (std::string&)) {
     std::vector<std::string> mappings;
-    for (const std::string& val: lst) {
+    for (std::string& val: lst) {
         std::string mapped = (this->*func)(val);
         mappings.emplace_back(mapped);
     }
     return mappings;
 }
 
+/**
+ * Unpack the synonym argument and assign to an AttrRef struct and the string identifier of the synonym
+ * @param synonym  synonym argument to be unpacked
+ * @param attrRef  pointer to the AttrRef
+ * @param synonymValue  pointer to the string identifier of the synonym
+ */
+void ResultClauseEvaluator::unpackSynonym(Argument &synonym, std::pair<string, AttrName> *attrRef, string *synonymValue) {
+    if (synonym.argumentType == ArgumentType::ATTR_REF) {
+        *attrRef = std::get<std::pair<string, AttrName>>(synonym.argumentValue);
+        *synonymValue = attrRef->first;
+    } else {
+        *synonymValue = std::get<std::string>(synonym.argumentValue);
+    }
+}
 
-string ResultClauseEvaluator::getVarRead(string stmtNumber) {
+/**
+ * Handle selection the attribute reference, update result table and index if needed
+ * @param attrRef  Attribute Reference of a synonym
+ * @param index  pointer originally storing the position for the current synonym, updated if new column is added
+ * @param resultTable  pointer to the result table to be used
+ */
+void ResultClauseEvaluator::updateTableForAttrReference(std::pair<string, AttrName> &attrRef, long* index, ResultTable* resultTable) {
+    auto header = resultTable->getHeader();
+    std::string(ResultClauseEvaluator::*func) (std::string&);
+    std::string name;
+    if (applyAttrRef(attrRef, &func, &name)) {
+        auto it1 = std::find(header->begin(), header->end(), name);
+        long newIndex = std::distance(header->begin(), it1);
+        if (it1 == header->end()) {
+            std::vector<std::string> newColumn = getMapping((*resultTable->getList())[*index], func);
+            resultTable->appendColumn(name, newColumn);
+        }
+        *index = newIndex;
+    }
+}
+
+
+string ResultClauseEvaluator::getVarRead(string& stmtNumber) {
     return pkb->statement.readStatements.getVariableName(stmtNumber);
 }
-string ResultClauseEvaluator::getVarPrinted(string stmtNumber) {
+string ResultClauseEvaluator::getVarPrinted(string& stmtNumber) {
     return pkb->statement.printStatements.getVariableName(stmtNumber);
 }
-string ResultClauseEvaluator::getProcByCall(string stmtNumber) {
+string ResultClauseEvaluator::getProcByCall(string& stmtNumber) {
     return pkb->statement.callStatements.getProcedureName(stmtNumber);
 }
--- Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/ResultClauseEvaluator.h
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/ResultClauseEvaluator.h
@@ -15,20 +15,25 @@ public:
     bool evaluateClause(ResultTable *resultTable) override;
 
 private:
-    void evaluateSelectedSynonyms(vector<int>*, ResultTable*);
+    void projectSelectedSynonyms(vector<int> *, ResultTable *);
 
-    bool applyAttrRef(std::pair<string, AttrName>& attrRef, std::string (ResultClauseEvaluator::**func) (std::string), std::string *name);
+    bool applyAttrRef(std::pair<string, AttrName> &attrRef, std::string (ResultClauseEvaluator::* *func)(std::string&),
+                      std::string *name);
 
-    std::vector<std::string> getMapping(std::vector<std::string> &, std::string (ResultClauseEvaluator::*func)(std::string));
+    std::vector<std::string> getMapping(std::vector<std::string> &, std::string (ResultClauseEvaluator::*func)(std::string&));
 
-    string getVarRead(string stmtNumber);
+    string getVarRead(string&);
 
-    string getVarPrinted(string stmtNumber);
+    string getVarPrinted(string&);
 
-    string getProcByCall(string stmtNumber);
+    string getProcByCall(string&);
 
-    void appendNewSynonym(string, ResultTable *);
+    void appendNewSynonym(string, ResultTable*);
+
+    void updateTableForAttrReference(std::pair<string, AttrName>&, long*, ResultTable*);
+
+    static void unpackSynonym(Argument&, std::pair<string, AttrName>*, string*);
+
 };
-
 
 #endif //SPA_RESULTCLAUSEEVALUATOR_H
