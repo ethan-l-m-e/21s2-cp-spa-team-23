b85cfa2d92e7138f1ef5fa23dd9ad3939b458188
tin_hong_wen <e03256506@u.nus.edu>
2022-03-25 11:38:42 +0800

    added possible bug fixes but commented out in PKB

--- Team23/Code23/src/spa/src/PKB/Statement/ReadStatementTable.h
+++ Team23/Code23/src/spa/src/PKB/Statement/ReadStatementTable.h
@@ -15,5 +15,7 @@ public:
     void addStatement(ReadNode *node);
 
     string getVariableName(string statementNumber);
+
+    //void clear() override;
 };
 
--- Team23/Code23/src/unit_testing/src/TestCacheOperator.cpp
+++ Team23/Code23/src/unit_testing/src/TestCacheOperator.cpp
@@ -2,85 +2,31 @@
 // Created by Tin Hong Wen on 11/3/22.
 //
 
-#include "QP_database/CFGOperator.h"
+#include "QP_database/ExecutionPathServices.h"
 #include "QP_Evaluator/TestUtilities.h"
 #include "catch.hpp"
 
 using namespace std;
-/*
-vector<unordered_map<int, NodeCFG*>> constructCFGForTesting() {
-    BranchCFG* branch1 = new BranchCFG(1);
-    NodeCFG* node2 = new NodeCFG(2);
-    NodeCFG* node3 = new NodeCFG(3);
-    NodeCFG* node4 = new NodeCFG(4);
-    NodeCFG* node5 = new NodeCFG(5);
-    LoopCFG* loop6 = new LoopCFG(6);
-    LoopCFG* loop7 = new LoopCFG(7);
-    NodeCFG* node8 = new NodeCFG(8);
-    NodeCFG* node9 = new NodeCFG(9);
-    LoopCFG* loop10 = new LoopCFG(10);
-    NodeCFG* node11 = new NodeCFG(11);
-    BranchCFG* branch12 = new BranchCFG(12);
-    NodeCFG* node13 = new NodeCFG(13);
-    NodeCFG* node14 = new NodeCFG(14);
 
-    branch1->setLeftNode(node2);
-    node2->setNextNode(node3);
-    node3->setNextNode(node4);
-    node4->setNextNode(node5);
 
-    branch1->setRightNode(loop6);
-    loop6->setNodeInLoop(loop7);
-    loop7->setNodeInLoop(node8);
-    node8->setNextNode(loop7);
-    loop7->setNextNode(loop6);
 
-    node5->setNextNode(node9);
-    loop6->setNextNode(node9);
-    node9->setNextNode(loop10);
-    loop10->setNodeInLoop(node11);
-    node11->setNextNode(branch12);
-    branch12->setLeftNode(node13);
-    branch12->setRightNode(node14);
-    node13->setNextNode(loop10);
-    node14->setNextNode(loop10);
+TEST_CASE("path exist between 2 nodes") {
+    PKB* pkb = constructPKBForAffectsClause();
 
-    NodeCFG* node15 = new NodeCFG(15);
-    NodeCFG* node16 = new NodeCFG(16);
-    NodeCFG* node17 = new NodeCFG(17);
-
-    node15->setNextNode(node16);
-    node16->setNextNode(node17);
-
-    unordered_map<int, NodeCFG*> allNodes;
-    allNodes[1] = branch1;
-    allNodes[2] = node2;
-    allNodes[3] = node3;
-    allNodes[4] = node4;
-    allNodes[5] = node5;
-    allNodes[6] = loop6;
-    allNodes[7] = loop7;
-    allNodes[8] = node8;
-    allNodes[9] = node9;
-    allNodes[10] = loop10;
-    allNodes[11] = node11;
-    allNodes[12] = branch12;
-    allNodes[13] = node13;
-    allNodes[14] = node14;
-    allNodes[15] = node15;
-    allNodes[16] = node16;
-    allNodes[17] = node17;
-    unordered_map<int, NodeCFG*> rootNodes;
-    return vector<unordered_map<int, NodeCFG*>>{allNodes, rootNodes};
 }
-*/
+
+
+
+/*
 TEST_CASE("if path exist between 2 nodes") {
     vector<unordered_map<int, NodeCFG*>> nodeMap = constructCFGForTesting();
     unordered_map<int, NodeCFG*> allNodes = nodeMap[0];
     int size = nodeMap.size();
 
     //different procedure
-    CFGOperator* cfgHandler = CFGOperator::getInstance();
+    ExecutionPathServices* cfgHandler = ExecutionPathServices::getInstance();
+    CHECK();
+
     CHECK(!cfgHandler->pathExistBetween(allNodes[4], allNodes[16], size));
     // direct
     CHECK(cfgHandler->pathExistBetween(allNodes[1], allNodes[2], size));
@@ -103,7 +49,7 @@ TEST_CASE("all possible right Nodes") {
     vector<unordered_map<int, NodeCFG*>> nodeMap = constructCFGForTesting();
     unordered_map<int, NodeCFG*> allNodes = nodeMap[0];
     int size = nodeMap.size();
-    CFGOperator* cfgHandler = CFGOperator::getInstance();
+    ExecutionPathServices* cfgHandler = ExecutionPathServices::getInstance();
     CHECK(unordered_set<string>{} == cfgHandler->gatherAllRightNodes(allNodes[17], size));
     CHECK(unordered_set<string>{"17"} == cfgHandler->gatherAllRightNodes(allNodes[16], size));
     CHECK(unordered_set<string>{"16", "17"} == cfgHandler->gatherAllRightNodes(allNodes[15], size));
@@ -120,7 +66,7 @@ TEST_CASE("all possible left Nodes") {
     vector<unordered_map<int, NodeCFG *>> nodeMap = constructCFGForTesting();
     unordered_map<int, NodeCFG *> allNodes = nodeMap[0];
     int size = nodeMap.size();
-    CFGOperator* cfgHandler = CFGOperator::getInstance();
+    ExecutionPathServices* cfgHandler = ExecutionPathServices::getInstance();
     CHECK(unordered_set<string>{} == cfgHandler->gatherAllLeftNodes(allNodes[15], size));
     CHECK(unordered_set<string>{"15"} == cfgHandler->gatherAllLeftNodes(allNodes[16], size));
     CHECK(unordered_set<string>{"15", "16"} == cfgHandler->gatherAllLeftNodes(allNodes[17], size));
@@ -130,9 +76,9 @@ TEST_CASE("all possible left Nodes") {
     CHECK(unordered_set<string>{"1", "6", "7", "8"} == cfgHandler->gatherAllLeftNodes(allNodes[8], size));
 }
 
-
 TEST_CASE("test lambda") {
     int arg = 3;
-    //bool (*function1)(int) = [](int x) { return CFGOperator::boolFoo(x); };
-    //CFGOperator::testFoo(function1);
+    //bool (*function1)(int) = [](int x) { return ExecutionPathServices::boolFoo(x); };
+    //ExecutionPathServices::testFoo(function1);
 }
+*/
