ce92860a378c185d19e5df36c10e6462e972ef7b
tin_hong_wen <e03256506@u.nus.edu>
2022-03-25 12:42:02 +0800

    added AffectsTEvaluator and operator but not tested, and new header for organisation

---
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/AffectsTClauseEvaluator.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Tin Hong Wen on 25/3/22.
+//
+
+#include "AffectsTClauseEvaluator.h"
+
+#include "QP_database/AffectsTOperator.h"
+
+bool AffectsTClauseEvaluator::isRelation(string left, string right) {
+    affectsTOperator->computeRelation(left, right);
+}
+
+unordered_set<std::string> AffectsTClauseEvaluator::getLeftSynonymValue(std::string right) {
+    return affectsTOperator->computeLHS(right);
+}
+
+unordered_set<std::string> AffectsTClauseEvaluator::getRightSynonymValue(std::string left) {
+    return affectsTOperator->computeRHS(left);
+}
+
+pair<DesignEntity, DesignEntity> AffectsTClauseEvaluator::getWildcardType () {
+    return make_pair(DesignEntity::ASSIGN, DesignEntity::ASSIGN);
+}
---
+++ Team23/Code23/src/spa/src/QP_Evaluator/ClauseEvaluators/ClauseEvaluatorCollection.h
@@ -0,0 +1,24 @@
+//
+// Created by Tin Hong Wen on 25/3/22.
+//
+
+#ifndef SPA_CLAUSEEVALUATORCOLLECTION_H
+#define SPA_CLAUSEEVALUATORCOLLECTION_H
+
+#endif //SPA_CLAUSEEVALUATORCOLLECTION_H
+
+#include "QP_Evaluator/ClauseEvaluators/PatternClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/FollowsClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/ParentClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/ResultClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/FollowsTClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/ParentTClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/ModifiesSClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/UsesSClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/ModifiesPClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/UsesPClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/NextClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/NextTClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/AffectsClauseEvaluator.h"
+#include "QP_Evaluator/ClauseEvaluators/AffectsTClauseEvaluator.h"
+
