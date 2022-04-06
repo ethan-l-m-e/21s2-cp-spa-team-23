b3fd97467998bd5d19dec813f5263a35006779e3
tin_hong_wen <e03256506@u.nus.edu>
2022-04-06 22:11:09 +0800

    reordered Design Extractor and fix pattern cond matching to be more declarative

--- Team23/Code23/src/spa/src/SourceProcessor/DesignExtractor.cpp
+++ Team23/Code23/src/spa/src/SourceProcessor/DesignExtractor.cpp
@@ -7,10 +7,10 @@ void DesignExtractor::Extract(Node* programNode){
     // check for semantics error
     SemanticsVerifier::detectDuplicateProcedure(programNode);
     SemanticsVerifier::detectCyclicCalls(programNode);
-    //extract variables and constants etc
-    EntityExtractor::extractAllEntities(programNode);
     //attaches each stmt node to a procedure
     EntityExtractor::assignProcedureToNodes(programNode,nullptr);
+    //extract variables and constants etc
+    EntityExtractor::extractAllEntities(programNode);
     //extract relationships
     RelationshipExtractor::extractRelationships(programNode);
 
